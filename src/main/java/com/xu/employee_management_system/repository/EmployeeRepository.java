733a74a05071d750fd3bd6059e4eab3c2da64aad
leng1031 <xu.leng@outlook.com>
2022-04-22 13:46:05 -0400

    add deleteEmployById and findEmployeeById

--- src/main/java/com/xu/employee_management_system/repository/EmployeeRepository.java
+++ src/main/java/com/xu/employee_management_system/repository/EmployeeRepository.java
@@ -4,6 +4,11 @@ import com.xu.employee_management_system.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional findEmployeeById(Long id);
 }
