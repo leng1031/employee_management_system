89a2d10e4cfa3edb39c945d09a3d239f0e273b88
leng1031 <xu.leng@outlook.com>
2022-04-22 13:58:27 -0400

    UserNotFoundException

---
+++ src/main/java/com/xu/employee_management_system/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.xu.employee_management_system.exception;
+
+public class UserNotFoundException extends RuntimeException{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+}
