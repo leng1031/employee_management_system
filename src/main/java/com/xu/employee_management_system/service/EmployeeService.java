c5f20886569468246df6b54e85a95d9f46ae2d95
leng1031 <xu.leng@outlook.com>
2022-04-22 14:08:05 -0400

    EmployeeRepository.java

---
+++ src/main/java/com/xu/employee_management_system/service/EmployeeService.java
@@ -0,0 +1,33 @@
+package com.xu.employee_management_system.service;
+
+import com.xu.employee_management_system.exception.UserNotFoundException;
+import com.xu.employee_management_system.model.Employee;
+import com.xu.employee_management_system.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+    public Employee findEmployeeById(Long id){
+        return employeeRepository.findEmployeeById(id)
+                .orElseThrow(() -> new UserNotFoundException("User by id " + id + " was not found"));
+    }
+
+    public void deleteEmployeeById(Long id){
+        employeeRepository.deleteEmployeeById(id);
+    }
+}
