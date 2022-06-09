64f868351c6946a117f3cfd288410d6391e4edfe
leng1031 <xu.leng@outlook.com>
2022-06-08 21:12:36 -0400

    EmployeeController.java

--- src/main/java/com/xu/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/xu/employee_management_system/controller/EmployeeController.java
@@ -1,10 +1,41 @@
 package com.xu.employee_management_system.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.xu.employee_management_system.model.Employee;
+import com.xu.employee_management_system.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/")
+@RequestMapping("/employee")
 public class EmployeeController {
 
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Employee>> findAllEmployee () {
+        List<Employee> employees = employeeService.findAllEmployees();
+        return new ResponseEntity<>(employees, HttpStatus.OK);
+    }
+
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Employee> findEmployeeById (@PathVariable Long id){
+        Employee employee = employeeService.findEmployeeById(id);
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Employee> addEmployee (@RequestBody Employee employee){
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return new ResponseEntity<>(employee, HttpStatus.CREATED);
+    }
 }
